59e9e7654870a077b589ba1949226fe1646752c6
Gautam Manek <gautammanek@Gautams-MacBook-Pro.local>
2022-10-31 12:14:01 +0800

    fix: fix cargo benchmarks

--- benches/generate_events.rs
+++ benches/generate_events.rs
@@ -14,9 +14,9 @@ fn event_generation(c: &mut Criterion) {
     // vary the qps and monitor the time required to send 1,000 events
     for qps in [1_000, 10_000, 100_000, 1_000_000].iter() {
         let mut nexmark_config = NexmarkConfig::default();
+        nexmark_config.first_event_rate = *qps;
         let nexmark_source = Arc::new(NexmarkSource::new(&nexmark_config));
-        group.bench_with_input(BenchmarkId::from_parameter(qps), qps, |b, &qps| {
-            nexmark_config.first_event_rate = qps as usize;
+        group.bench_with_input(BenchmarkId::from_parameter(qps), qps, |b, &_qps| {
             b.to_async(tokio::runtime::Runtime::new().unwrap())
                 .iter(|| {
                     create_generators_for_config::<ChaCha8Rng>(&nexmark_config, &nexmark_source)
@@ -27,14 +27,14 @@ fn event_generation(c: &mut Criterion) {
     // since the qps is 10_000, gold standard is 0.1s per iter
     for num_gen in 1..10 {
         let mut nexmark_config = NexmarkConfig::default();
+        nexmark_config.num_event_generators = num_gen as usize;
+        nexmark_config.first_event_rate = 10_000;
+        nexmark_config.max_events = 1_000;
         let nexmark_source = Arc::new(NexmarkSource::new(&nexmark_config));
         group.bench_with_input(
             BenchmarkId::from_parameter(num_gen),
             &num_gen,
-            |b, &num_gen| {
-                nexmark_config.num_event_generators = num_gen as usize;
-                nexmark_config.first_event_rate = 10_000;
-                nexmark_config.max_events = 1_000;
+            |b, &_num_gen| {
                 b.to_async(tokio::runtime::Runtime::new().unwrap())
                     .iter(|| {
                         create_generators_for_config::<ChaCha8Rng>(&nexmark_config, &nexmark_source)
@@ -46,16 +46,16 @@ fn event_generation(c: &mut Criterion) {
     // since the qps is 10_000, gold standard is 0.1s per iter
     for byte_size in [100, 200, 300, 400, 500] {
         let mut nexmark_config = NexmarkConfig::default();
+        nexmark_config.first_event_rate = 10_000;
+        nexmark_config.max_events = 1_000;
+        nexmark_config.avg_auction_byte_size = byte_size;
+        nexmark_config.avg_person_byte_size = byte_size;
+        nexmark_config.avg_bid_byte_size = byte_size;
         let nexmark_source = Arc::new(NexmarkSource::new(&nexmark_config));
         group.bench_with_input(
             BenchmarkId::from_parameter(byte_size),
             &byte_size,
-            |b, &byte_size| {
-                nexmark_config.first_event_rate = 10_000;
-                nexmark_config.max_events = 1_000;
-                nexmark_config.avg_auction_byte_size = byte_size;
-                nexmark_config.avg_person_byte_size = byte_size;
-                nexmark_config.avg_bid_byte_size = byte_size;
+            |b, &_byte_size| {
                 b.to_async(tokio::runtime::Runtime::new().unwrap())
                     .iter(|| {
                         create_generators_for_config::<ChaCha8Rng>(&nexmark_config, &nexmark_source)
