82054f065e3f047e27dc3b6e46a0a56347ff31b8
lmatz <lmatz823@gmail.com>
2023-07-12 20:25:48 +0800

    add q12 and fix view (#25)

--- risingwave/create_views.sql
+++ risingwave/create_views.sql
@@ -1,11 +1,11 @@
-CREATE VIEW bid 
+CREATE VIEW bid
 AS
-SELECT (bid).* FROM nexmark WHERE event_type = 2;
+SELECT (bid).auction, (bid).bidder, (bid).price, (bid).channel, (bid).url, (bid).extra, p_time, date_time FROM nexmark WHERE event_type = 2;
 
-CREATE VIEW auction 
+CREATE VIEW auction
 AS
-SELECT (auction).* FROM nexmark WHERE event_type = 1;
+SELECT (auction).id, (auction).item_name, (auction).description, (auction).initial_bid, (auction).reserve, (auction).expires, (auction).seller, (auction).category, (auction).extra, p_time, date_time FROM nexmark WHERE event_type = 1;
 
-CREATE VIEW person 
+CREATE VIEW person
 AS
-SELECT (person).* FROM nexmark WHERE event_type = 0;
+SELECT (person).id, (person).name, (person).email_address, (person).credit_card, (person).city, (person).state, (person).extra, p_time, date_time FROM nexmark WHERE event_type = 0;
