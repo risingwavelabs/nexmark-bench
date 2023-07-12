2480a27382dc95d24e906b1c6f84f3fbba6d412a
lmatz <lmatz823@gmail.com>
2023-07-12 20:22:25 +0800

    add q12 and fix view

--- risingwave/create_sinks.sql
+++ risingwave/create_sinks.sql
@@ -177,9 +177,16 @@ CREATE SINK nexmark_q10 AS
 
 
 CREATE SINK nexmark_q12 AS
-    SELECT bidder, count(*) as bid_count, window_start, window_end
-    FROM TUMBLE(bid, p_time, INTERVAL '10' SECOND)
-    GROUP BY bidder, window_start, window_end
+    SELECT bidder,
+           count(*) as bid_count,
+           window_start,
+           window_end
+    FROM
+           TUMBLE(bid, p_time, INTERVAL '10' SECOND)
+    GROUP BY 
+           bidder,
+           window_start,
+           window_end
     WITH ( connector = 'blackhole', type = 'append-only', force_append_only = 'true');
 
 
